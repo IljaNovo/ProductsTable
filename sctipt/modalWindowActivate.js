03694ca8f433109af3be4215e34c277b5ec7f69c
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-05-26 12:28:59 +0300

    add module window

---
+++ sctipt/modalWindowActivate.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('input[type="submit"][name="add"]').click( function(event){
+		event.preventDefault();
+		$('#overlay').fadeIn(400, 
+		 	function(){
+				$('#add_new_modal_form') 
+					.css('display', 'block')
+					.animate({opacity: 1, top: '50%'}, 200);  
+		});
+	});
+	
+	$('#add_new_modal_close, #overlay').click( function() {
+		$('#add_new_modal_form')
+			.animate({opacity: 0, top: '45%'}, 200,
+				function() {
+					$(this).css('display', 'none');
+					$('#overlay').fadeOut(400);
+				}
+			);
+	});
+});
