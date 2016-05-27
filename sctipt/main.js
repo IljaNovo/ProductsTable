7d06e020835940d0f1faca179c70b86b46c5f6a2
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-05-27 17:02:56 +0300

    add function adding form

--- sctipt/main.js
+++ sctipt/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
-       $("#add_new_modal_form input[name='addOrUpdate']").click(function () {
-            formBuilder.createForm(); 
+        
+       addEventOpenForm("#delete_modal_form", "input[type='submit'][name='delete']");
+       addEventCloseModal("#delete_modal_form", "#delete_modal_close");
+    
+       addEventOpenForm("#add_new_modal_form", "input[type='submit'][name='add']");
+       addEventCloseModal("#add_new_modal_form", "#add_new_modal_close");
+    
+       addEventOpenForm("#edit_modal_form", "input[type='submit'][name='edit']");
+       addEventCloseModal("#edit_modal_form", "#edit_modal_close");
+    
+       addEventOpenForm("#edit_modal_form", "#main-section .name-product a");
+       
+    
+    
+       $("#add_new_modal_form input[type='submit'][name='addOrUpdate']").click(function () {
+            formBuilder.createForm();
+            closeForm("#add_new_modal_form");
    }); 
 });
--- sctipt/modalWindowActivate.js
+++ sctipt/modalWindowActivate.js
@@ -1,53 +1,33 @@
-$(document).ready(function() {
-    var nameForm = "";
-    var nameClose = "#delete_modal_close";
-    
-    $('input[type="submit"][name="edit"]')
-        .add('input[type="submit"][name="delete"]')
-        .add('input[type="submit"][name="add"]')
-        .add('#products .name-product')
+function addEventOpenForm(nameModalForm, selectorButton) {
+    $(selectorButton)
         .click(function(event) {
         event.preventDefault();
-        
-        switch(event.target.name) {
-            case "edit":
-                nameForm = "#edit_modal_form";
-                nameClose = "#edit_modal_close";
-                break;
-            case "delete":
-                nameForm = "#delete_modal_form";
-                nameClose = '#delete_modal_close';    
-                break;
-            case "add":
-                nameForm = "#add_new_modal_form";
-                nameClose = '#add_new_modal_close';
-                break;
-            default:
-                nameForm = "#edit_modal_form";
-                nameClose = "#edit_modal_close";
-                break;
-        }
-        closeGenerate();
-        
         $('#overlay').fadeIn(400, 
 		 	function() {
-				$(nameForm)
+				$(nameModalForm)
                     .css("display", "block")
 					.animate({opacity: 1, top: '50%'}, 200);  
 		});
 	});
+}
 	
-    function closeGenerate() {
-	   $("#overlay")
-           .add(nameClose)
-           .click(function() {
-        $(nameForm)
-			.animate({opacity: 0, top: '45%'}, 200,
-				function() {
-					$(this).css("display", "none");
-					$("#overlay").fadeOut(400);
-				}
-			);
-	   });
-    }
-});
+function addEventCloseModal(nameModalForm, selector) {
+    $("#overlay")
+        .add(selector)
+        .click(function() {
+            closeForm(nameModalForm);
+    });
+}
+
+
+function closeForm (nameModalForm) {
+    $(nameModalForm)
+        .animate({opacity: 0, top: '45%'}, 200,
+        function() {
+            $(this).css("display", "none");
+            $("#overlay").fadeOut(400);
+        }
+    );
+}
+
+
